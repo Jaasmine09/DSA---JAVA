44388f79423c15851ee632e7722d2a4bae61944f
Jasmine <jaasmine2001@gmail.com>
2024-03-06 07:37:22 +0530

    variables and Datatypes

---
+++ Variables & Data Types/output.java
@@ -0,0 +1,7 @@
+public class output {
+    public static void main(String args[]){
+        System.out.print("Hello Jasmine!");
+        System.out.println(" , Welcome to the Coding Class");// println is used to print in next line
+        System.out.println("Have a  nice day!");
+    }
+}
